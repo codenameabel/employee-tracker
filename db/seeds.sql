a033a7eaa84d36bb475759439888e4f4fa703a5b
Your Name <you@example.com>
2021-06-13 16:06:09 -0700

    added employee table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees (first_name, last_name)
+VALUES
+    ('John', 'Doe'),
+    ('Mike', 'Chan'),
+    ('Ashley', 'Rodriguez'),
+    ('Kevin', 'Tupik'),
+    ('Malia', 'Brown'),
+    ('Sarah', 'Lourd'),
+    ('Tom', 'Allen');  
